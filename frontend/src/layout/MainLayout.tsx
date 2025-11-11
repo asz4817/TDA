9a48f4827d6b8dd2e59f1cfbe0a29e06e6fc946e
asz4817 <133913519+asz4817@users.noreply.github.com>
2025-11-11 12:51:54 -0600

    build

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import { Outlet, useLocation } from "react-router-dom"
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import Divider from "../components/Divider";
+// import Divider from "../components/Divider";
 import Hero from "../components/Hero";
-import PerformanceCards from "../components/PerformanceCards";
+// import PerformanceCards from "../components/PerformanceCards";
 
 
 const HomePage = () => {
