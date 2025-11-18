f63b47edeacc60d9f47c8e8abc8f277d2270e6cb
Cassidy Lee <lee.cassidy.007@gmail.com>
2025-11-18 03:07:44 -0600

    mobile nav bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,11 +3,28 @@ import TDALogo from "../assets/Tda_Logo.svg";
 import Line from "../assets/Line 1.svg";
 import profile from "../assets/profile.svg";
 import "../App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Lottie, { type LottieRefCurrentProps } from "lottie-react";
+import Menu from "../assets/Menu.json";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
 	const [scrolling, setScrolling] = useState(true);
 	const [scrollTop, setScrollTop] = useState(0);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+	const menu = useRef<HTMLDivElement>(null);
+
+	const location = useLocation();
+
+	useEffect(() => {
+		if (isOpen) {
+			setIsOpen(false);
+			lottieRef.current?.playSegments([70, 140], true);
+		}
+	}, [location]);
 
 	useEffect(() => {
 		function onScroll() {
@@ -34,7 +51,7 @@ export default function Navbar() {
 				transition: "opacity 0.5s ease-out",
 			}}
 		>
-			<div className="relative mx-auto w-[75%] md:mt-20 mt-10 flex justify-between">
+			<div className="relative mx-auto w-[93%] md:w-[75%] md:mt-20 mt-10 flex justify-between">
 				{/* Left: Logo */}
 				<div className="flex items-center gap-[10%] w-[75%]">
 					<NavLink to="/" className="items-center">
@@ -78,7 +95,7 @@ export default function Navbar() {
 						className={({ isActive }) =>
 							`transition hover:text-gray-400 duration-400 ${
 								isActive ? "font-semibold" : "text-white"
-							}`
+							} hidden md:block`
 						}
 						style={{
 							fontFamily: "unbounded",
@@ -107,9 +124,93 @@ export default function Navbar() {
 						CONNECT WITH US
 					</a>
 
-					<img src={Line} alt="Line" className="h-[50%] md:h-[3vw]" />
-					<img src={profile} alt="profile" width={"11%"} />
-
+					<img
+						src={Line}
+						alt="Line"
+						className="h-[50%] md:h-[3vw] mr-[-4px] md:mr-[0]"
+					/>
+					<img
+						className="hidden md:block"
+						src={profile}
+						alt="profile"
+						width={"11%"}
+					/>
+					<Lottie
+						className="cursor-pointer w-[30px] md:hidden"
+						animationData={Menu}
+						// loop={true}
+						autoplay={false}
+						lottieRef={lottieRef}
+						onLoopComplete={() =>
+							lottieRef.current?.goToAndStop(70, true)
+						}
+						onClick={() => {
+							lottieRef.current?.setSpeed(1.3);
+							if (isOpen) {
+								lottieRef.current?.playSegments(
+									[70, 140],
+									true
+								);
+							} else {
+								lottieRef.current?.playSegments([0, 70], true);
+							}
+							setIsOpen(!isOpen);
+						}}
+					/>
+					<div
+						ref={menu}
+						className="absolute top-[100%] right-[2%]"
+						style={{
+							visibility: isOpen ? "visible" : "hidden",
+							opacity: isOpen ? 1 : 0,
+							transform: isOpen
+								? "translateY(50%)"
+								: "translateY(20%)",
+							transition:
+								"visibility 0.5s ease-out, opacity 0.5s ease-in, transform 0.5s ease-in-out",
+							transitionDelay: ".55s",
+						}}
+					>
+						<ul className="text-right space-y-4">
+							<li>
+								<NavLink
+									to="/contactUs"
+									className={({ isActive }) =>
+										`transition hover:text-gray-400 duration-400 ${
+											isActive
+												? "font-semibold"
+												: "text-white"
+										} `
+									}
+									style={{
+										fontFamily: "unbounded",
+										fontSize: "14px",
+									}}
+								>
+									CONTACT US
+								</NavLink>
+							</li>
+							<li>
+								{" "}
+								<NavLink
+									to="/txdc2026"
+									className={({ isActive }) =>
+										`transition hover:text-gray-300 ${
+											isActive
+												? "font-semibold"
+												: "text-white"
+										}`
+									}
+									style={{
+										fontFamily: "unbounded",
+										fontSize: "14px",
+									}}
+								>
+									TXDC 2026
+								</NavLink>
+							</li>
+						</ul>
+					</div>
 					{/* <User size={20} className="cursor-pointer hover:text-gray-300" /> */}
 				</div>
 			</div>
